3b2ede362a35943e8f78013e53f1cf3b090971b7
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-06-08 02:46:43 +0500

    Add files via upload

--- IOT_interupt_project/IOT_interupt_project.ino
+++ IOT_interupt_project/IOT_interupt_project.ino
@@ -6,7 +6,7 @@
 #define GSM_TX_PIN 11
 int pin[5]={2,3,4,5,6};
 int relay = LED_BUILTIN;
-
+int reset_button=8;//
 bool interupt1_state =0;
 bool interupt2_state=0;
 bool interupt3_state =0;
@@ -16,9 +16,14 @@ bool flag;// for interutp indication
 uint16_t prev_time ;
 uint16_t curr_time ;
 uint16_t d_time=1000;
+uint16_t last_time ;
+//uint16_t curr_time ;
+//uint16_t d_time=1000;
+int  last_state=0;
 String command;
 String command_s;
 
+int state;
 SoftwareSerial gsmSerial(GSM_RX_PIN, GSM_TX_PIN);
 void setup() {
   // put your setup code here, to run once:
@@ -27,11 +32,12 @@ pinMode(pin[i],INPUT_PULLUP);
 //attachInterrupt(digitalPinToInterrupt(pin[i]),Interuptcall(pin[i]),CHANGE);
 }
 pinMode(relay,OUTPUT);
+pinMode(reset_button,OUTPUT);
 Serial.begin(9600);
 gsmSerial.begin(9600);
 
 }
-
+// function to send message on mobile from gsm
 void gsmMsg(const String& message) {
   gsmSerial.println("AT+CMGF=1"); // Set SMS 
   delay(10);
@@ -42,32 +48,33 @@ void gsmMsg(const String& message) {
   gsmSerial.write(26); // End of message character
 }
 
+// interupt function for interupt detection and 
 void Interuptcall(int interupt_no){
   switch(interupt_no){
   case 2:interupt1_state = 1;
     sendmsg(1);
     gsmMsg("Iterupt at port 1");
-    gsmCall();
+    //gsmCall();
     break;
   case 3:interupt2_state = 1;
     sendmsg(2);
     gsmMsg("Iterupt at port 2");
-    gsmCall();
+    //gsmCall();
     break;
   case 4:interupt3_state = 1;
     sendmsg(3);
     gsmMsg("Iterupt at port 3");
-    gsmCall();
+    //gsmCall();
     break;
   case 5:interupt4_state = 1;
     sendmsg(4);
     gsmMsg("Iterupt at port 4");
-    gsmCall();
+    //gsmCall();
     break;
   case 6:interupt5_state = 1;
     sendmsg(5);
     gsmMsg("Iterupt at port 5");
-    gsmCall();
+    //gsmCall();
     break;
 
   case 10:
@@ -82,6 +89,7 @@ void Interuptcall(int interupt_no){
   }
 }
 
+// to display interupt detection on remote site i.e serial monitor
 void sendmsg(int pin_no){
   
   String txt =(pin_no!=10) ? "Interupt Call on Port no " + String(pin_no) : " Reset call Removing Interupt" ;   
@@ -91,9 +99,11 @@ void sendmsg(int pin_no){
   
   }
 
-void gsmCall() {
+void gsmCall() {// gsm module call on mobile fuctnion
   gsmSerial.println("ATD+1234567890;"); // phone number
 }
+
+
 
 void sendResetNotification(const String& location) {
   gsmSerial.println("AT+CMGF=1"); // Set SMS text mode
@@ -104,6 +114,8 @@ void sendResetNotification(const String& location) {
   gsmSerial.print(location);
   gsmSerial.write(26); // End of message character
 }
+
+
 
 void relay_control(bool value){
   digitalWrite(relay,value);
@@ -127,11 +139,13 @@ void relay_call(bool states){
   relay_control(states);
   }
   if(states==0){
+    
   Serial.println("Relay Deatcive Mobile Call Recived");
   relay_control(states);
   }
-  }
+  delay(500);}
   
+
 bool call() //call from mobile etc
 {
   Serial.println("Disabling interupt  ");
@@ -140,28 +154,59 @@ bool call() //call from mobile etc
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+int rst_rd = digitalRead(reset_button);
+
+if(rst_rd!=last_state){
+  last_time=millis();
+  Serial.print("last time ");
+  Serial.println(last_time);
+  
+  }
+
+if((millis()-last_time)>2000){ //  reset button should pressed 0.1 sec; to reset to avaoid ambigous reset  
+  if(rst_rd!=state){
+    state=rst_rd;
+    Serial.print("button pressed rst value  ");
+    Serial.println(state);
+    
+    
+    if(state){
+      Interuptcall(10);
+       relay_call(0);
+   
+    String reset_sms="Reset recived at time x"; //+ String(getGSMTimestamp());
+    sendResetNotification(reset_sms);}
+    }
+  
+  last_state=rst_rd;}
+
+
 if(digitalRead(pin[0])==0 && interupt1_state==0 ){
   Interuptcall(pin[0]);
   }
   
 
-else if(digitalRead(pin[1])==0&& interupt2_state==0){
+if(digitalRead(pin[1])==0&& interupt2_state==0){
   Interuptcall(pin[1]);
   }
 
-else if(digitalRead(pin[2])==0&& interupt3_state==0){
+if(digitalRead(pin[2])==0&& interupt3_state==0){
   Interuptcall(pin[2]);
   }
 
-else if(digitalRead(pin[3])==0&& interupt4_state==0){
+if(digitalRead(pin[3])==0&& interupt4_state==0){
   Interuptcall(pin[3]);
   }
-else if(digitalRead(pin[4])==0&& interupt5_state==0){
+if(digitalRead(pin[4])==0&& interupt5_state==0){
   Interuptcall(pin[4]);
   }
 
 
-if(gsmSerial.available()) {
+  
+
+if(gsmSerial.available())// for gsm reset 
+{
  command = gsmSerial.readStringUntil('\n');
     command.trim();
     if (command == "RESET") {
@@ -202,12 +247,12 @@ if(interupt5_state||
     interupt2_state||
     interupt1_state ){
       relay_call(1);
-      delay(1000);//after call wait for 10 send
+      delay(500);//after call wait for 10 send
       flag=1;
     }
 
     
-    else if(command =="RESET" &&flag ==1){
+    else if(command =="RESET" &&flag ==1){//receive reset from gsm 
     Serial.println("the interutp is being disable ");
     relay_call(0);
     Interuptcall(10);
