5da8fb6fdd0c21e2d38980bbd4b5d9a2509f2655
Humayun Sheikh <58903281+humayunsiraj123@users.noreply.github.com>
2023-11-04 21:53:05 +0500

    Add files via upload

--- smart_garden/smart_garden.ino
+++ smart_garden/smart_garden.ino
@@ -19,11 +19,12 @@ char pass[] = "humayunsj789";
 
 #define SCL 22//for i2c
 #define SDA 21//for i2c
-#define DHTPIN 17//dht pin
+#define DHTPIN 25//dht pin
 #define soil_sensor_1 34   // Pin connected to soil moisture sensor
-#define soil_sensor_2 36   // Pin connected to soil moisture sensor
-#define pump_1 4     // Pin connected to relay control
-#define pump_2 5     // Pin connected to relay control
+#define soil_sensor_2 35   // Pin connected to soil moisture sensor
+#define pump_1 19     // Pin connected to relay control
+#define pump_2 23
+// Pin connected to relay control
 
 
 bool pump_1_state = 0;
@@ -41,7 +42,7 @@ int a=1;
 void soilMoistureSensor() {
   a=a+10;
   //int soilMoisture = ++a;//analogRead(soil_sensor_2);
-  int soilMoisture_1 =a; //analogRead(soil_sensor_1);//analogRead(soil_sensor_2);
+  int soilMoisture_1 =analogRead(soil_sensor_1);//analogRead(soil_sensor_2);
   float soil_per_1 =  (  ( (soilMoisture_1/4095.00) * 100 ) );//
   Serial.print("soil_sensor_1.................... ");
   Serial.println(soil_per_1);
@@ -55,29 +56,36 @@ void soilMoistureSensor() {
   Blynk.virtualWrite(V0, soil_per_1);     // Send soil moisture 1 to V0 on the Blynk app
   Blynk.virtualWrite(V1, soil_per_2);    // Send soil moisture 2 to V1 on the Blynk app
 
+   lcd.setCursor(0,1);
+  lcd.print("S_1:");
+  lcd.print(soil_per_1);
 
-if (soil_per_1<soil_threshold) 
+  lcd.setCursor(8, 1);
+  lcd.print("S_2:");
+  lcd.print(soil_per_2);
+
+if (soil_per_1>soil_threshold) 
   {
     digitalWrite(pump_1, 0);  // Turn on the water pump
     Serial.println("plant 1 low water ... pump 1 on");
     Blynk.virtualWrite(V4,1);  // Send status message to V9 on the Blynk app
   }
 
-  else if (soil_per_1 > soil_threshold) 
+  else if (soil_per_1 < soil_threshold) 
   {  // Adjust the threshold value based on your soil moisture sensor
     digitalWrite(pump_1, 1);  // Turn off pump 1
     Serial.println("plant 1 has enough mositures pump 1 is off.");
     Blynk.virtualWrite(V4,0 );  // Send status message to V9 on the Blynk app
   }
 
-if (soil_per_2<soil_threshold) 
+if (soil_per_2>soil_threshold) 
   {
     digitalWrite(pump_2, 0);  // Turn on the water pump
     Serial.println("plant 2 low water ... pump 2 on");
     Blynk.virtualWrite(V4,1);  // Send status message to V9 on the Blynk app
   }
 
-  else if (soil_per_2 > soil_threshold) 
+  else if (soil_per_2 < soil_threshold) 
   {  // Adjust the threshold value based on your soil moisture sensor
     digitalWrite(pump_2, 1);  // Turn off pump 1
     Serial.println("plant 2 has enough mositures pump 2 is off.");
@@ -105,11 +113,11 @@ void DHT11sensor() {
     return;
   }
 
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 0);
   lcd.print("T:");
   lcd.print(t);
 
-  lcd.setCursor(8, 1);
+  lcd.setCursor(8, 0);
   lcd.print("H:");
   lcd.print(h);
   Blynk.virtualWrite(V2, t);  // Send temperature to V2 on the Blynk app
